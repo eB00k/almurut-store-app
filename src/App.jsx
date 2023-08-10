cb5f738e98807ffb3612783fe4cd80347808dd57
Dastan Akatov <107542637+eB00k@users.noreply.github.com>
2023-08-10 14:23:17 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/layouts/Navbar";
+
+function App() {
+  return (
+    <>
+      <section className="hero">
+        <Navbar/>
+      </section>
+    </>
+  );
+}
+
+export default App;
